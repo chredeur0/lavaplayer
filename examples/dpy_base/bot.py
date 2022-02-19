bc331afbcf4bb989b9ca6d3425f5c1b0b95eaefb
chredeur <50155089+chredeur0@users.noreply.github.com>
2022-02-19 18:15:30 +0100

    Update bot.py

--- examples/dpy_base/bot.py
+++ examples/dpy_base/bot.py
@@ -111,6 +111,12 @@ async def repeat(ctx: commands.Context, stats: bool):
     await lavalink.repeat(ctx.guild.id, stats)
     await ctx.send("Repeated the queue.")
 
+@bot.command()
+async def filter(self, ctx: commands.Context):
+    filters = lavaplayer.Filters()
+    filters.rotation(0.2)
+    await lavalink.filters(ctx.guild.id, filters)
+    await ctx.send("Filter applied.")
 
 @bot.event
 async def on_socket_raw_receive(data):
